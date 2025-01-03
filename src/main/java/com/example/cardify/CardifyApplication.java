23797644dc9a5002fdb4694fe2d54a8ae2a30f53
shubhkaran30546 <73439286+shubhkaran30546@users.noreply.github.com>
2025-01-02 19:07:06 -0700

    Initial commit

---
+++ src/main/java/com/example/cardify/CardifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cardify/CardifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
