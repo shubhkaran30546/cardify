f915c9d80e4d1997aa0a0454aa0f97b261102f22
shubhkaran30546 <73439286+shubhkaran30546@users.noreply.github.com>
2025-01-08 15:41:34 -0700

    commit

--- cardify-frontend/src/Signup.js
+++ cardify-frontend/src/Signup.js
@@ -8,7 +8,7 @@ const Signup = () => {
     const handleEmailSignup = () => {
         navigate("/email-signup"); // Navigate to the email signup page
     };const handleGoogleSignup = () => {
-        navigate("https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&client_id=632656753280-mlqvu6hvm9tdi6voallkspr03diat6qt.apps.googleusercontent.com&scope=openid%20profile%20email&state=jea1_TnW5Dx1yKQD5K-nnJ854yzlc_yekO3AadljLZ0%3D&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Flogin%2Foauth2%2Fcode%2Fgoogle&nonce=3sxJaEJP4V-VWZoqubb63Qo9uIrn0tSGbFdikmhzEw8&service=lso&o2v=2&ddm=1&flowName=GeneralOAuthFlow"); // Navigate to the email signup page
+        navigate("/login/oauth2/code/google"); // Navigate to the email signup page
     };
 
     return (
