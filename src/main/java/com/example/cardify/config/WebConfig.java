94536475b9f1dd90b83e1c97a3945ed25b0f94c4
ArshArora <arsharora0388@gmail.com>
2025-05-24 14:52:21 -0600

    Moved frontend build contents to static for deployment

--- src/main/java/com/example/cardify/config/WebConfig.java
+++ src/main/java/com/example/cardify/config/WebConfig.java
@@ -15,8 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/{spring:\\w+}")
                 .setViewName("forward:/index.html");
-        registry.addViewController("/**/{spring:\\w+}")
-                .setViewName("forward:/index.html");
         registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
                 .setViewName("forward:/index.html");
     }
