f31f37a5130d51749623a31dd887562833a97935
ArshArora <arsharora0388@gmail.com>
2025-05-24 16:45:55 -0600

    Fix SPA routing and update static assets for deployment

--- src/main/java/com/example/cardify/config/WebConfig.java
+++ src/main/java/com/example/cardify/config/WebConfig.java
@@ -1,6 +1,9 @@
 package com.example.cardify.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,17 +12,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/index.html");
+    }
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploaded_images/**")
                 .addResourceLocations("file:uploaded_images/");
     }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{spring:[^\\.]+}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/**/{spring:[^\\.]+}")
-                .setViewName("forward:/index.html");
-    }
 }
-
