02f46435877425e889e0ea36f8bdbf558513992a
ArshArora <arsharora0388@gmail.com>
2025-05-26 14:21:28 -0600

    Fix SPA routing and update static assets for deployment

--- src/main/java/com/example/cardify/config/WebConfig.java
+++ src/main/java/com/example/cardify/config/WebConfig.java
@@ -11,21 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/{spring:\\w+}")
-//                .setViewName("forward:/index.html");
-//        registry.addViewController("/**/{spring:\\w+}")
-//                .setViewName("forward:/index.html");
-//        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-//                .setViewName("forward:/index.html");
-//    }
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // Exclude static resources from forwarding
-        registry.addViewController("/{spring:^(?!static|js|css|api|uploaded_images).*$}")
+        registry.addViewController("/{spring:\\w+}")
                 .setViewName("forward:/index.html");
-        registry.addViewController("/*/{spring:^(?!static|js|css|api|uploaded_images).$}")
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
                 .setViewName("forward:/index.html");
     }
 
