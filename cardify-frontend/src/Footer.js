3465a002c5868741677fe9f4ee65e1d417bffdee
Sanket Lamba <101916088+sanketlamba@users.noreply.github.com>
2025-05-30 20:18:06 +0000

    Fri, May 30, 2025, 2:18 PM -06:00

--- cardify-frontend/src/Footer.js
+++ cardify-frontend/src/Footer.js
@@ -4,13 +4,22 @@ import './Footer.css';
 import {useNavigate} from "react-router-dom";
 
 const Footer = () => {
-    const XIcon = ({ size = 24, color = "white" }) => (
-        <svg width={size} height={size} viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g>
-            <path d="M854.5 0H1108L698.5 524.5L1167 1227H807.5L525.5 812.5L203.5 1227H-55L388.5 661.5L-34 0H334L587.5 381.5L854.5 0ZM789 1105.5H885.5L282.5 114.5H180.5L789 1105.5Z" fill={color}/>
-            </g>
+    const XIcon = ({ size = 22, color = "white", style = {} }) => (
+        <svg
+            width={size}
+            height={size}
+            viewBox="0 0 1200 1227"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ display: "block", ...style, verticalAlign: "middle" }}
+        >
+            <path
+            d="M854.5 0H1108L698.5 524.5L1167 1227H807.5L525.5 812.5L203.5 1227H-55L388.5 661.5L-34 0H334L587.5 381.5L854.5 0ZM789 1105.5H885.5L282.5 114.5H180.5L789 1105.5Z"
+            fill={color}
+            />
         </svg>
-    );
+        );
+
 
     const navigate = useNavigate();
     const handleNavigateHome = (section) => {
@@ -22,12 +31,12 @@ const Footer = () => {
                 <div className="footer-logo">
                     <img src="logo.png" alt="Cardify Logo" />
                 </div>
-                <div className="footer-social">
+                <div className="footer-social" style={{ display: "flex", gap: "18px", alignItems: "center" }}>
                     {/* <a href="#"><FaYoutube /></a> */}
                     {/* <a href="#"><FaFacebookF /></a> */}
                     {/* <a href="#"><FaTwitter /></a> */}
-                    <a href="#" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                        <XIcon size={24} color="white" />
+                    <a href="#" style={{ display: "flex", alignItems: "center" }}>
+                        <XIcon size={22} color="white" />
                     </a>
                     {/* <a href="#"><FaInstagram /></a>? */}
                     <a href="#"><FaLinkedinIn /></a>
